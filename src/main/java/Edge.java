ba818c34b4040def1c45255d7e1b7e341cea1352
r <b.alrami@icloud.com>
2025-10-24 23:53:38 +0500

    feat: edge class

--- src/main/java/Edge.java
+++ src/main/java/Edge.java
@@ -1,4 +1,52 @@
-package PACKAGE_NAME;
+package graph;
 
-public class Edge {
+public class Edge implements Comparable<Edge> {
+    private final String from;
+    private final String to;
+    private final int weight;
+
+    public Edge(String from, String to, int weight) {
+        this.from = from;
+        this.to = to;
+        this.weight = weight;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    @Override
+    public int compareTo(Edge other) {
+        return Integer.compare(this.weight, other.weight);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("{from: %s, to: %s, weight: %d}", from, to, weight);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof Edge)) return false;
+        Edge other = (Edge) obj;
+        return weight == other.weight &&
+                ((from.equals(other.from) && to.equals(other.to)) ||
+                        (from.equals(other.to) && to.equals(other.from)));
+    }
+
+    @Override
+    public int hashCode() {
+        String min = from.compareTo(to) < 0 ? from : to;
+        String max = from.compareTo(to) < 0 ? to : from;
+        return min.hashCode() * 31 + max.hashCode() * 31 + Integer.hashCode(weight);
+    }
 }
